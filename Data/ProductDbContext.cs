c23f0b7cd787ab62555b45161ed897d2c442b56d
AjaVelaCode <andrijavela@gmail.com>
2021-09-17 02:40:51 +0200

    Add project files.

---
+++ Data/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ Entities/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Entities
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(50)]
+        public string Manufacturer { get; set; }
+        public decimal Price { get; set; }
+        public int EngineNumber { get; set; }
+        public string EngineType { get; set; }
+        public string FuelType { get; set; }
+    }
+}
